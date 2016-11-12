dd3bd24f308bf2f480ad0e3578cd2ab6cd79e8b7
Deniz Candas <dnzcandas@gmail.com>
2016-11-12 17:57:07 +0100

    Add files via upload

--- data.py
+++ data.py
@@ -4,6 +4,8 @@ from itertools import groupby
 import cPickle as pickle
 import random
 
+
+
 class CQuery:
 	def __init__(self, mid = "", title = "", text = "", opts = []):
 		self.id = mid
@@ -12,9 +14,18 @@ class CQuery:
 		self.opts = opts
 	@staticmethod
 	def from_case(mid, case):
-		print case
-		return CQuery(mid,"Title",str(case),["a","b","c"])
-	
+		a = ""
+		for i in pid.keys():
+			if pid[i] == case[-1][0]:
+				a = i
+
+		return CQuery(mid,"Case ID: " + str(case[0]), "Plant: " + a + " | Throughput time: "  + str(case[-1][3] / 24) + " days " 
+			+ str(case[-1][3] % 24) + " hours" + " | User Count: " + str(case[-1][4]), [die[x[0]] for x in case[1]])
+	#str(case[1:-1])
+	#"Number of Activities per Case: " + str(case[-1][2])
+	#		return CQuery(mid,"Case ID: " + str(case[0]), ["Plant: " + a,"Throughput time: "  + str(case[-1][1]), 
+	#		"Hours between first and last Activity (Throughput Time): " + str(case[-1][3]),"User Count: " + str(case[-1][4])], [die[x[0]] for x in case[1]])
+
 	def get_dict(self):
 		return {"id":self.id, "title":self.title, "text":self.text, "opts": self.opts}
 	
@@ -24,12 +35,13 @@ class CQuery:
 def read_data():
 	cases = None
 	kis = None
-	with open("data/cases.csv") as f:
+	with open("cases.csv") as f:
 		cases = [row for row in csv.reader(f.readlines(), delimiter=";")]
-	
-	with open("data/kis.csv") as f:
+	#with open("throughput.csv") as f:
+	#	through = [row for row in csv.reader(f.readlines(), delimiter=";")]
+	with open("kis.csv") as f:
 		kis = [row for row in csv.reader(f.readlines(), delimiter=";")]
-	
+	#print through
 	return cases[1:], kis[1:]
 
 def process_data():
@@ -42,53 +54,41 @@ def process_data():
 	
 	eid = dict(reversed(x) for x in enumerate(es))
 	
+	
 	ckeys = list(set(e[0] for e in cases))
 	
 	ces = dict((key, [(s[1], s[2]) for s in sorted([g for g in group], key = lambda x: x[2])]  ) for key, group in groupby(((c[0], eid[c[1]], c[2]) for c in cases), lambda c: c[0]))
 	
 	cs = [(key, ces[key], ks[key]) for key in ckeys] 
 	
+	#print cs
 	return cs, eid, pid
-
-def get_edges(cases):
-	edgd = dict()
-	for k, case in cases.items():
-		for i,e in enumerate(case[0]):
-			if i == 0:
-				continue
-			mk = (case[0][i-1][0], case[0][i][0])
-			if mk not in edgd:
-				edgd[mk] = 0
-			edgd[mk] += 1
 	
-	edsort = [(x[0],x[1]) for x in sorted(edgd.items(), key = lambda x: x[1], reverse=True)]
-	return edsort
-
 def save_data():
 	cs, eid, pid = process_data()
-	cases = dict((c[0], c[1:]) for c in cs)
-	edg = get_edges(cases)
-	data = (cs, eid, pid, cases, edg)
-	pickle.dump(data, open("data/data.p", "wb"))
+	data = (cs, eid, pid)
+	pickle.dump(data, open("data.p", "wb"))
 	pass
 
 def load_data():
-	data = pickle.load(open("data/data.p", "rb"))
-	cs, eid, pid, cases, edg = data
+	data = pickle.load(open("data.p", "rb"))
+	cs, eid, pid = data
 	#print cs
-	return cs, eid, pid, cases, edg
+	return cs, eid, pid
 
 #save_data()
-cs, eid, pid, cases, edg = load_data()
-#print cs
+cs, eid, pid = load_data()
+die = {y:x for x,y in eid.iteritems()} #die die die
 
-#print len(cs)
+lolol = CQuery.from_case("123",random.choice(cs))
+print eid
+print die
 
 def sample_query():
-	mcs = random.choice(cs)
-	return CQuery.from_case(mcs[0],mcs)
+	return CQuery.from_case("123",random.choice(cs))
+	#return CQuery("123","Topkek","Ayy lmaoo",["Topkek","Tanzen","Oww yeah"])
 	
-
+	
 	
 
 
